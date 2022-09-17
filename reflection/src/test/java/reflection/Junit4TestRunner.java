b149250b3644cb317ec097607b8532c73f806de8
Nam Minjeong <nmj5012@naver.com>
2022-09-17 16:14:51 +0900

    feat: 자바 리플렉션 사용하기 미션

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,6 +10,15 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
+        Junit4Test clazzObj = clazz.getConstructor().newInstance();
+
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+
+        for (Method method : declaredMethods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazzObj);
+            }
+        }
     }
 }
