29b8e381b83347fcfcbb0d273a6ff7ab9986b8c6
Nam Minjeong <nmj5012@naver.com>
2022-09-27 16:44:39 +0900

    test: 4단계
    - stage3에서 구현한 기능을 기본적으로 제공한다.
    - 생성자 파라미터로 패키지명을 받아서 클래스를 찾는 ClassPathScanner를 구현한다.
    - @Service, @Repository가 존재하는 클래스만 객체로 생성한다.
    - 객체에서 @Inject를 붙인 필드만 필터하고 타입에 맞는 객체(bean)를 찾아서 대입(assign)한다.
    - DI에서 관리하는 객체(bean)를 찾아서 반환한다.

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,13 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.reflections.Reflections;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -9,16 +16,67 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    private DIContainer(final Set<Class<?>> classes) {
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> targetClasses = getTargetClasses(rootPackageName);
+        return new DIContainer(targetClasses);
+    }
+
+    private static Set<Class<?>> getTargetClasses(String rootPackageName) {
+        Set<Class<?>> classes = new HashSet<>();
+        Reflections reflections = new Reflections(rootPackageName);
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        return classes;
+    }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+            .map(this::getObject)
+            .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object getObject(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException("빈 생성 중 에러가 발생하였습니다.");
+        }
+    }
+
+    private void setFields(Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            setField(bean, field);
+        }
+    }
+
+    private void setField(Object bean, Field field) {
+        try {
+            if (field.isAnnotationPresent(Inject.class)) {
+                field.set(bean, getBean(field.getType()));
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+    public <T> T getBean(final Class<T> clazz) {
+        return (T)beans.stream()
+            .filter(bean -> clazz.isInstance(bean))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("빈을 찾을 수 없습니다."));
+
     }
 }
