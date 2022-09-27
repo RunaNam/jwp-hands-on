dd4a3f1878ce4b0be507af97f0a3c0a7e1dfef27
Nam Minjeong <nmj5012@naver.com>
2022-09-27 16:09:23 +0900

    test: 3단계
    - 생성자 파라미터로 Set<Class<?>>를 전달하자.
    - 전달 받은 클래스를 객체로 생성한다.
    - 객체의 내부 필드의 타입에
    맞는 객체(baen)를 찾아서 대입(assign)한다.
    - DI에서 관리하는 객체(bean)를 찾아서 반환한다.

--- di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
@@ -1,6 +1,10 @@
 package nextstep.study.di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +14,51 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+            .map(this::getObject)
+            .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object getObject(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException("빈 생성 중 에러가 발생하였습니다.");
+        }
+    }
+
+    private void setFields(Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            setField(bean, field);
+        }
+    }
+
+    private void setField(Object bean, Field field) {
+        try {
+            if (field.get(bean) == null) {
+                field.set(bean, getBean(field.getType()));
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+    public <T> T getBean(final Class<T> clazz) {
+        return (T)beans.stream()
+            .filter(bean -> clazz.isInstance(bean))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("빈을 찾을 수 없습니다."));
     }
 }
