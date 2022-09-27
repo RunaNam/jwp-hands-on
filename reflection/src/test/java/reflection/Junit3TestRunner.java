62b0a91f8ca2bab20ee17e182d07026a2483ab1f
Minjeong Nam <54093525+RunaNam@users.noreply.github.com>
2022-09-27 16:48:03 +0900

    feat: 자바 리플렉션 사용하기 미션 (#4)

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -8,6 +10,14 @@ class Junit3TestRunner {
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
+        Junit3Test clazzObj = clazz.getConstructor().newInstance();
+
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if(method.getName().startsWith("test")){
+                method.invoke(clazzObj);
+            }
+        }
     }
 }
